89826df3843d641cac17610497a9754ce15f3576
草木灰 <jsbianxiang@hotmail.com>
2026-07-09 03:31:30 +0800

    Add feed sources to feeds.conf.default

---
+++ diy-part1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git" >> feeds.conf.default
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git" >> feeds.conf.default
+echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git" >> feeds.conf.default
